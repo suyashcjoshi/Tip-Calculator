d052b64655f75bca05d4576e244a96d6214eb7cc
Suyash Joshi <suyash@suyashjoshi.com>
2013-09-18 22:39:32 -0700

    Initial Commit

---
+++ Tip Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tip Calculator
+//
+//  Created by Suyash Joshi on 9/18/13.
+//  Copyright (c) 2013 Suyash Joshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tip Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tip Calculator
+//
+//  Created by Suyash Joshi on 9/18/13.
+//  Copyright (c) 2013 Suyash Joshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
